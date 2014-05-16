57c3f7c151d2c166c480b678e06756edf11fd3b1
ice cream <tiffanymblount@gmail.com>
2014-05-15 19:24:21 -0600

    Probably the final commit.
    The code correctly identifies the test words. It could be cleaned up a
    bit more, but is still very much readable.

--- ABCProblem/src/abcreader/AlphaBlockRing.java
+++ ABCProblem/src/abcreader/AlphaBlockRing.java
@@ -1,5 +1,5 @@
 /*
- * A circular array of Blocks
+ * An array of blocks based loosely off a circular array.
  * 
  * The default size of the array is 20, 
  * but this can be changed.
@@ -19,7 +19,6 @@ public class AlphaBlockRing {
 	private int _size = 20;
 	
 	private int _head = 0;
-	private int _tail = _head;
 	
 	public AlphaBlockRing() {
 		initBlocks();
@@ -38,27 +37,39 @@ public class AlphaBlockRing {
 	public void visitBlocks() {
 		int index = _head;
 		int wordIndex = 0;
-		String word = "box"; //match, try "bxo" to see no match
+		String word = "soap"; //match, try "bxo" to see no match
 		
 		
 		while (true) {
 			Block<Character, Character> block = _blocks.get(index);
 			
 			if (word.charAt(wordIndex) == block.getA()) {
-				if (wordIndex != word.length() - 1) {
+				if (indexIsInBounds(wordIndex, word)) {
 					wordIndex++;
 				} else {
+					System.out.println("No match");
 					break;
 				}
 				
 				if (word.charAt(wordIndex) == block.getB()) {
-					if (wordIndex != word.length() - 1) {
+					if (indexIsInBounds(wordIndex, word)) {
 						wordIndex++;
 					}
 				} else {
+					System.out.println("No match");
 					break;
 				}
+			} else {
+				//keep moving until you find the first letter
+				//we know the first letter will always match
+				//because the blocks are guaranteed to have all letters of the alphabet
+				index++;
+				
+				if (indexIsInBounds(wordIndex, word)) {
+					wordIndex++;
+				}
 			}
+			
 				if (wordIndex == word.length() - 1) {
 					System.out.println("Match");
 					break;
@@ -67,9 +78,14 @@ public class AlphaBlockRing {
 				if (index == _size - 1) {
 					index = 0; //wraparound
 				}
+				
 				index++;
 				block = _blocks.get(index);
 		}
+	}
+	
+	private boolean indexIsInBounds(int currLength, String word) {
+		return (currLength != word.length() - 1);
 	}
 	
 	public static void main(String[] args) {
